0230d0998d3a44e4ece61947aa270434edb4e058
Juan Gonzalez <jj151358@gmail.com>
2026-05-20 22:51:11 -0600

    bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,7 @@ import os
 # ========================================
 # CAMBIA SOLO ESTOS 3 VALORES
 # ========================================
-TELEGRAM_TOKEN = "8641134190:AAHH96wQ5H3_TJjYfKK1meCJ5Cu-hMl4EuI"
-CHANNEL_ID = "@OfertasJG"
-ADMITAD_BASE64 = "ZGc4bHhlOEZHSVROZXlzeE9XcWpEbU05akxjNmUwOjh6QWRMNW5LbDBSdFE2RXpqcnVuWWlSWkZaeHhSZg=="
-WEBSITE_ID = "2943808"
+
 DESCUENTO_MINIMO = 5
 # ========================================
 async def diagnostico():
